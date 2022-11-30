0ee75e56ef15835f7c1e52f6aacdc4f7d2a0cdf9
kwangminini <rhkdals7362@gmail.com>
2022-12-01 01:04:22 +0900

    init webpack config

---
+++ pathTest.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+console.log("__dirname ::: ", __dirname);
+console.log("path ::: ", path);
+const pathTest = path.resolve(__dirname, "test");
+
+console.log("pathTest ::: ", pathTest);
