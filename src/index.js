f6d862550e3597f9a3df594095001fdbb4cfd58b
kwangminini <rhkdals7362@gmail.com>
2022-12-04 23:11:58 +0900

    test webpack css loader setting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "../index.css";
+
 function component() {
   const element = document.createElement("div");
   element.innerHTML = "Hello world";
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  mode: "none",
   target: "node",
 };
